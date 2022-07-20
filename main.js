677715c28a013bc5a8ad0d6c087ded6ee01244ae
jihyun517 <50868983+jihyun517@users.noreply.github.com>
2022-07-20 15:11:03 +0900

    lodash find

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
-import {getType, user} from './getType.js'
+import _ from 'lodash'
 
-console.log(getType(user))
+const userA = [
+  {userId: '1', name: 'Jihyun'},
+  {userId: '2', name: 'Mong'}
+]
+const userB = [
+  {userId: '1', name: 'Jihyun'},
+  {userId: '3', name: 'Loopy'}
+]
+
+const userC = userA.concat(userB)
+console.log('concat', userC)
+console.log('uniqBy', _.uniqBy(userC, 'userId'))
+
+console.log('unionBy', _.unionBy(userA, userB, 'userId'))
